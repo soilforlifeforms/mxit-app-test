7e6c8a414ebd240629e3e369d2529065ae634fd7
soilforlifeforms <rvermooten@gmail.com>
2014-02-17 14:53:47 +0200

    intial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MxitAppTest::Application.config.secret_token = '65e368083c18233c5d420c76c26fe0c5d79ec5006c38ce106a7727c34eb9e50110bb516018e85ba316fe5778eccb3e706ca1c74db0394e9532bb73e96562de4e'
